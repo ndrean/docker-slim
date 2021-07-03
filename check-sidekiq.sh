fc22dfa3309391b5a8a2804dafc189b95c124c46
neven <nevendrean@yahoo.fr>
2021-07-03 21:11:30 +0200

    testing sidekiq health check

--- check-sidekiq.sh
+++ check-sidekiq.sh
@@ -7,7 +7,8 @@ SIDEKIQ_REDIS=redis://user:secretpwd@redisdb:6379 sidekiqmon > txt
 started=$(cat txt | grep Started | wc -l)
 
 if [[ $started -ne 1 ]]; then
-   echo "1"
+   echo "down"
+   return 0
 else
-   echo "0"
+   echo "up"
 fi;
