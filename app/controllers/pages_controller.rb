0764252d7dd466a2c30d1b771acd47906711365a
neven <nevendrean@yahoo.fr>
2021-07-06 07:48:29 +0200

    refact errors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,21 @@ class PagesController < ApplicationController
   class Error < StandardError
   end
 
-  def home    
+  def home
     # <- test Redis database
     p "Redis db:  #{REDIS.ping}"
-    # <- test Sidekiq/Redis connection
-    p "Redis-Sidekiq: #{Sidekiq.redis { |conn| conn.connection[:id] }}"
+    # <- test Sidekiq/Redis connection (github/sidekiq/lib/sidekiq.rb)
+    p "Redis-Sidekiq: #{Sidekiq.redis { |con| con.connection[:id] }}"
+
     # PSQL <- test PG connection
-    ActiveRecord::Base.connection.execute("SELECT 1") 
+    begin
+      one = ActiveRecord::Base.connection.execute("SELECT 1").getvalue(0,0)
+      STDOUT.puts "PG is UP" if (one ==1)
+      raise PagesController::Error.new('PG down') if (one != 1)
+    rescue => e
+      STDOUT.puts e.message
+    end
+    
     #<- check Sidekiq
     SidekiqHelper.check
       
@@ -20,42 +28,49 @@ class PagesController < ApplicationController
 
   def start_workers
     begin
-      if (Sidekiq::Stats.new().processes_size > 0)
-        # background WORKER with Sidekiq
-        HardWorker.perform_async
-        # ACTIVE_JOB with Sidekiq (intializer with REDIS_URL, config.active_job.queue_adapter)
-        HardJob.perform_later 
-        return render json: { status: 200 }
-      end
-      raise PagesController::Error.new('Workers Sidekiq down')
-    
+      raise PagesController::Error.new('Workers Sidekiq down') if (Sidekiq::Stats.new().processes_size == 0)
+      # Worker with Sidekiq
+      HardWorker.perform_async
+      # Active_Job with Sidekiq
+      HardJob.perform_later 
+      render json: { status: 200 }
     rescue => e
       STDERR.puts e.message
-      return render json: { status: 500}
+      render json: { status: 500}
     end
   end
 
   def get_counters
-    cPG = Counter.last
-    cRed = REDIS.get('compteur')
+    begin
+      cPG = Counter.last
+      cRed = REDIS.get('compteur')
+      countPG = (cPG == nil) ? 0 : cPG.nb
+      countRedis = (cRed == '') ? 0 : cRed
+      render json: {
+        countPG: countPG,
+        countRedis: countRedis,
+        status: :ok
+      }
 
-    countPG = (cPG == nil) ? 0 : cPG.nb
-    countRedis = (cRed == '') ? 0 : cRed
-
-    render json: {
-      countPG: countPG,
-      countRedis: countRedis,
-      status: :ok
-    }
+    rescue => e
+      STDERR.puts e.message
+      render json: { status: 500}
+    end
+    
   end
 
   def create
     # console
     # p params
-    Counter.create!(nb: params[:countPG])
-    REDIS.set("compteur", params[:countRedis])
+    begin
+      Counter.create!(nb: params[:countPG])
+      REDIS.set("compteur", params[:countRedis])
+      render json: { status: :created }
+    rescue => e
+      STDOUT.puts e.message
+      render json: {status: 500}
+    end
     
-    render json: { status: :created }
   end
 
 end
--- app/helpers/sidekiq_helper.rb
+++ app/helpers/sidekiq_helper.rb
@@ -1,22 +1,18 @@
 module SidekiqHelper
    require 'sidekiq/monitor'
-   
    class Error < StandardError
    end
    
-
    def self.check
       begin
-         # github/sidekiq/lib/sidekiq/monitor.rb
-         Sidekiq::Monitor::Status.new.display(section="processes")
          # cf gtihub/lib/sidekiq/Sidekiq/api.rb
          size = Sidekiq::Stats.new().processes_size
          raise SidekiqHelper::Error.new("Sidekiq down") if (size == 0)
+         STDOUT.puts "Sidekiq is UP" if (size>0)
+         # github/sidekiq/lib/sidekiq/monitor.rb
+         Sidekiq::Monitor::Status.new.display(section="processes")
       rescue => e
          STDERR.puts e.message
       end
-      # github/sidekiq/lib/sidekiq.rb
-      puts Sidekiq.redis { |con| con.connection[:id] }
    end
-   
 end
