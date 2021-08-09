ec5a45efcfab7a1b38bf4190d853075df33f89e6
neven <nevendrean@yahoo.fr>
2021-08-09 11:07:14 +0200

    cleaned

--- app/channels/counter_channel.rb
+++ app/channels/counter_channel.rb
@@ -2,17 +2,14 @@ class CounterChannel < ApplicationCable::Channel
   def subscribed
     # pubsub
     stream_from "counter_channel"
-    
   end
 
   def receive(data)
     # rebroadcasting the received message to any other connected client
     ActionCable.server.broadcast('counter_channel',data.as_json)
   end
-
   
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
     stop_all_streams
   end
 end
